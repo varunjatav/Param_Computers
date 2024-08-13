885d0b5669c6351a587057bb6d1db3750b7c847f
Varun Jatav <mohit.mohit979@gmail.com>
2024-08-13 19:00:54 +0530

    about us changes

--- src/screens/AboutUs.jsx
+++ src/screens/AboutUs.jsx
@@ -15,7 +15,7 @@ const AboutUs = () => {
         </SlideInFromLeft>
         <div className="border-4 border-teal-400 w-32 m-auto rounded-md"></div>
         <SlideInFromRight>
-        <p className="text-lg text-gray-500 pt-5 m-auto text-center lg:text-left">
+        <p className="text-lg text-gray-500 pt-5 m-auto text-center">
           Welcome to Param Computers, pioneering computer eductaion since 2001.
           We empower individual for success in a digital landscape. With 23
           years of expertise, We've guided countless students in mastering
